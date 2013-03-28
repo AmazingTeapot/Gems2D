ef7c18d152e078a1e9da94373698dda783e50a29
Héctor Candón Arenas <hecktor45@gmail.com>
2013-03-28 21:19:29 +0100

    Some improvements
    Improvejades algunes coses de la representació, també he canviat
    l'heurístic (ara get beneficis retorna el benefici total) i canviat
    algunes coses de notació (la goal state ha de tornar false, l'heurístic
    en negatiu, etc)

--- Gems2D/MovingImageLayer.h
+++ Gems2D/MovingImageLayer.h
@@ -3,19 +3,28 @@
 #include "Camera.h"
 #include "Sprite.h"
 
+
+/* Class: Moving Image Layer
+   Brief: With this kind of layer it is possible to display an image of any size as background. The particularity of this layer is
+		  that, if the image is bigger than the screen size, it will move progressively as the level advances, so the image "ends"
+		  at the same time than the level.
+		  It is possible to superpose two or more of this layers to create a profundity sensation. */
+
 class MovingImageLayer : public Layer {
 
 public:
 
-MovingImageLayer();
+	/* Creates a void Moving Image Layer */
+	MovingImageLayer();
 
-MovingImageLayer(Image* i);
+	/* Creates a Moving Image Layer an
+	MovingImageLayer(Image* i);
 
-~MovingImageLayer();
+	~MovingImageLayer();
 
-virtual void setLayer (string path);
+	virtual void setLayer (string path);
 
-virtual void draw (sf::RenderWindow& App);
+	virtual void draw (sf::RenderWindow& App);
 
 private:
 
